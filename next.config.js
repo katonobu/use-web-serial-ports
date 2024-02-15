be5fd7e5d25794980379da925d208a3cf72dc456
Nobuo Kato <katonobu4649@gmail.com>
2024-02-16 01:56:05 +0900

    change ignoreDuringBuilds to false

--- next.config.js
+++ next.config.js
@@ -11,7 +11,8 @@ const nextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+//    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false,
   },  
 };
 
